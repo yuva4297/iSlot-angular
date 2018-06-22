74c2c83b867c9fd4d64ddbe6a1633f1044552536
yuva4297 <yuvaammu3@gmail.com>
2018-06-22 19:15:53 +0530

    Multiple select dropdown is added

--- iSlot-angular/src/app/registration/pages/register-interviewer/register-interviewer.component.ts
+++ iSlot-angular/src/app/registration/pages/register-interviewer/register-interviewer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-interviewer',
@@ -6,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-interviewer.component.css']
 })
 export class RegisterInterviewerComponent implements OnInit {
-
-  constructor() { }
+  optionsModel: number[];
+    myOptions: IMultiSelectOption[];
+    registerInterviewerForm : FormGroup;
+  constructor() {
+    this.registerInterviewerForm =new FormGroup(
+      {
+        username: new FormControl('',Validators.required),
+        email: new FormControl('', [Validators.required, Validators.pattern(/^[a-z|A-Z][a-z|A-Z|0-9|]+@virtusa.com/)]),
+      }
+    )
+   }
 
   ngOnInit() {
+    this.myOptions = [
+      { id: 1, name: 'Option 1' },
+      { id: 2, name: 'Option 2' },
+  ];
   }
+  onChange() {
+    console.log(this.optionsModel);
+}
   // Material Select Initialization
 
 
