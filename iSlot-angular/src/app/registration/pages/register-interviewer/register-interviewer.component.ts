06bbedface02baf312164331bff59c080fec413f
yuva4297 <yuvaammu3@gmail.com>
2018-06-22 14:35:17 +0530

    Login and register as interviewer UI

---
+++ iSlot-angular/src/app/registration/pages/register-interviewer/register-interviewer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-interviewer',
+  templateUrl: './register-interviewer.component.html',
+  styleUrls: ['./register-interviewer.component.css']
+})
+export class RegisterInterviewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  // Material Select Initialization
+
+
+}
--- iSlot-angular/src/app/registration/registration.routes.ts
+++ iSlot-angular/src/app/registration/registration.routes.ts
@@ -1,6 +1,16 @@
 import { Route } from "@angular/router";
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterInterviewerComponent } from "./pages/register-interviewer/register-interviewer.component";
 export const routes: Route[] = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register-interviewer',
+        component: RegisterInterviewerComponent
+    },
     {
         path: '',
         component: LandingPageComponent
