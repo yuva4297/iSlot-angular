21e7e2dfe35727ff18bc22bb5a11523970ad4ab8
soundarya <soundaryadandu77@gmail.com>
2018-06-22 17:58:44 +0530

    Admin dashboard and interviewer dashboard UI is done

--- iSlot-angular/src/app/app.routes.ts
+++ iSlot-angular/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Route } from "@angular/router";
 
 export const routes: Route[] = [ 
     {
+        path: 'interviewer',
+        loadChildren: 'app/interviewer/interviewer.module#InterviewerModule'
+    },
+    {
         path: 'admin',
         loadChildren: 'app/admin/admin.module#AdminModule'
         
     },
+
     {
         path: '',
         loadChildren: 'app/registration/registration.module#RegistrationModule'
---
+++ iSlot-angular/src/app/interviewer/interviewer.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router/router";
+import { InterviewerDashboardComponent } from "./interviewer-dashboard/interviewer-dashboard.component";
+
+export const routes: Route[] = [
+    {
+        path: '',
+        component: InterviewerDashboardComponent
+    }
+ ]
