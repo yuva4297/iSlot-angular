ba76046504a15aadaf36e08bdcb469920d756980
soundarya <soundaryadandu77@gmail.com>
2018-06-23 17:11:40 +0530

    drive registration with validation is done

--- iSlot-angular/src/app/admin/admin.module.ts
+++ iSlot-angular/src/app/admin/admin.module.ts
@@ -4,10 +4,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from "@angular/router";
 import { routes } from "./admin.routes";
 import { DriveregistrationComponent } from './driveregistration/driveregistration.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MultiselectDropdownModule } from "angular-2-dropdown-multiselect";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    MultiselectDropdownModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [DashboardComponent, DriveregistrationComponent,]
