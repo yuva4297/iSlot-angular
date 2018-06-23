c26ebb5ceeeaf6668b9513d3516ab15e1b725259
yuva4297 <yuvaammu3@gmail.com>
2018-06-23 17:52:02 +0530

    Admin registration is done with validation

--- iSlot-angular/src/app/registration/pages/login/login.component.ts
+++ iSlot-angular/src/app/registration/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup
+  constructor() { 
+    this.loginForm = new FormGroup(
+    {
+      email: new FormControl('',[Validators.required, Validators.pattern(/^[a-z|A-Z][a-z|A-Z|0-9|]+@virtusa.com/)]),
+      password: new FormControl('',[Validators.required])
+    
+    }
+  )
+  }
 
   ngOnInit() {
   }
